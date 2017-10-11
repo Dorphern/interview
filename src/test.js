9df507ff6f4d02534b1e03d05b1226c9cdcbe275
Kristoffer Olesen <kristofferdo@gmail.com>
2017-10-11 11:42:38 +0200

    Add assignment including basic test file, with benchmarking.

---
+++ src/test.js
@@ -0,0 +1,48 @@
+// Imports
+const test = require('tape')
+const index = require('./index')
+
+test('Sample test', (t) => {
+
+  // Add stuff to be tested here
+  t.equal(index(1, 2), '1, 2')
+
+  t.end()
+})
+
+test.skip('Benchmark testing', (t) => {
+
+  console.log(benchmark(() => (123).toString()))
+  console.log(benchmark(() => (123).toString(2)))
+
+  t.end()
+})
+
+
+
+// Benchmark method
+// -----------------------------------------------------------------
+const benchmark = (fn) => {
+  const start = process.hrtime()
+
+  let runs = 0
+  const minRuns = 5
+  const maxRuns = 100000
+  const maxSeconds = 1
+
+  for (
+    let i = 0;
+    i < minRuns || i < maxRuns && maxSeconds > process.hrtime(start)[0];
+    i++
+  ) {
+    fn()
+    runs++
+  }
+
+  const [elapsedSeconds, elapsedNs] = process.hrtime(start)
+
+  // Return elapsed time in ms
+  return (elapsedSeconds * 1000 + elapsedNs / 1000000) / runs
+}
+
+
